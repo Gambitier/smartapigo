ff8a2709e54fc5b95d10c4e3f8eb69c44e172db1
Surendra Kumar <skkarwasra02@gmail.com>
2024-10-17 21:50:55 +0530

    Holding structure updated

--- portfolio.go
+++ portfolio.go
@@ -6,23 +6,27 @@ import (
 
 // Holding is an individual holdings response.
 type Holding struct {
-	Tradingsymbol      string `json:"tradingsymbol"`
-	Exchange           string `json:"exchange"`
-	ISIN               string `json:"isin"`
-	T1Quantity         string `json:"t1quantity"`
-	RealisedQuantity   string `json:"realisedquantity"`
-	Quantity           string `json:"quantity"`
-	AuthorisedQuantity string `json:"authorisedquantity"`
-	ProfitAndLoss      string `json:"profitandloss"`
-	Product            string `json:"product"`
-	CollateralQuantity string `json:"collateralquantity"`
-	CollateralType     string `json:"collateraltype"`
-	Haircut            string `json:"haircut"`
+	Tradingsymbol      string  `json:"tradingsymbol"`
+	Exchange           string  `json:"exchange"`
+	ISIN               string  `json:"isin"`
+	T1Quantity         int32   `json:"t1quantity"`
+	RealisedQuantity   int32   `json:"realisedquantity"`
+	Quantity           int32   `json:"quantity"`
+	AuthorisedQuantity int32   `json:"authorisedquantity"`
+	ProfitAndLoss      float32 `json:"profitandloss"`
+	Product            string  `json:"product"`
+	CollateralQuantity int32   `json:"collateralquantity"`
+	CollateralType     string  `json:"collateraltype"`
+	Haircut            float32 `json:"haircut"`
+	AveragePrice       float32 `json:"averageprice"`
+	LTP                float32 `json:"ltp"`
+	SymbolToken        string  `json:"symboltoken"`
+	Close              float32 `json:"close"`
+	PNLPercentage      float32 `json:"pnlpercentage"`
 }
 
 // Holdings is a list of holdings
 type Holdings []Holding
-
 
 // GetHoldings gets a list of holdings.
 func (c *Client) GetHoldings() (Holdings, error) {
